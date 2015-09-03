a173b8f93be0efa1dce0c1bfc95ab29ac324c879
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-09-03 18:42:19 -0300

    HealthCheckJob refactoring

--- src/main/java/io/galeb/services/healthchecker/sched/HealthCheckJob.java
+++ src/main/java/io/galeb/services/healthchecker/sched/HealthCheckJob.java
@@ -16,8 +16,15 @@
 
 package io.galeb.services.healthchecker.sched;
 
+import static io.galeb.core.model.BackendPool.PROP_HEALTHCHECK_CODE;
+import static io.galeb.core.model.BackendPool.PROP_HEALTHCHECK_HOST;
+import static io.galeb.core.model.BackendPool.PROP_HEALTHCHECK_PATH;
+import static io.galeb.core.model.BackendPool.PROP_HEALTHCHECK_RETURN;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ExecutionException;
 
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
@@ -33,31 +40,26 @@ import io.galeb.core.model.BackendPool;
 import io.galeb.core.model.Entity;
 import io.galeb.core.model.Farm;
 import io.galeb.core.model.Rule;
-import io.galeb.core.model.collections.BackendCollection;
 import io.galeb.core.model.collections.BackendPoolCollection;
 import io.galeb.core.services.AbstractService;
 import io.galeb.services.healthchecker.HealthChecker;
-import io.galeb.services.healthchecker.Tester;
+import io.galeb.services.healthchecker.testers.TestExecutor;
 
 @DisallowConcurrentExecution
 public class HealthCheckJob implements Job {
 
-    private Tester tester;
+    private TestExecutor tester;
     private Optional<Logger> logger = Optional.empty();
     private Farm farm;
     private DistributedMap<String, Entity> distributedMap;
 
     @SuppressWarnings("unchecked")
-    @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-
-        final JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
-
+    private void init(final JobDataMap jobDataMap) {
         if (!logger.isPresent()) {
             logger = Optional.ofNullable((Logger) jobDataMap.get(AbstractService.LOGGER));
         }
         if (tester==null) {
-            tester = (Tester) jobDataMap.get(HealthChecker.TESTER_NAME);
+            tester = (TestExecutor) jobDataMap.get(HealthChecker.TESTER_NAME);
             tester.setLogger(logger);
         }
         if (farm==null) {
@@ -66,79 +68,96 @@ public class HealthCheckJob implements Job {
         if (distributedMap==null) {
             distributedMap = (DistributedMap<String, Entity>) jobDataMap.get(AbstractService.DISTRIBUTEDMAP);
         }
+    }
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+
+        init(context.getJobDetail().getJobDataMap());
 
         logger.ifPresent(log -> log.info("=== " + this.getClass().getSimpleName() + " ==="));
+
         final BackendPoolCollection backendPoolCollection = (BackendPoolCollection) farm.getCollection(BackendPool.class);
-        final BackendCollection backendCollection = (BackendCollection) farm.getCollection(Backend.class);
-
-        if (!backendPoolCollection.isEmpty() && !backendCollection.isEmpty()) {
-            backendCollection.stream()
-                .forEach(backend -> {
-
-                    final Optional<Entity> backendPoolOptional = backendPoolCollection.stream()
-                                                        .filter(pool -> pool.getId().equals(backend.getParentId()))
-                                                        .findAny();
-
-                    if (backendPoolOptional.isPresent()) {
-
-                        BackendPool backendPool = (BackendPool) backendPoolOptional.get();
-
-                        final String url = backend.getId();
-                        String returnType = "string";
-                        final Backend.Health lastHealth = ((Backend) backend).getHealth();
-
-                        String host = (String) backendPool.getProperty(BackendPool.PROP_HEALTHCHECK_HOST);
-                        if (host==null) {
-                            Optional<Entity> rule = farm.getCollection(Rule.class).stream()
-                                    .filter(r -> backendPool.getId().equalsIgnoreCase((String) r.getProperty(Rule.PROP_TARGET_ID)))
-                                    .findAny();
-                            host = rule.isPresent() ? rule.get().getParentId() : url;
-                        }
-
-                        String healthCheckPath = (String) backendPool.getProperty(BackendPool.PROP_HEALTHCHECK_PATH);
-                        if (healthCheckPath==null) {
-                            healthCheckPath = System.getProperty(HealthChecker.PROP_HEALTHCHECKER_DEF_PATH, "/");
-                        }
-
-                        final String fullUrl = url + healthCheckPath;
-
-                        String expectedReturn = (String) backendPool.getProperty(BackendPool.PROP_HEALTHCHECK_RETURN);
-                        if (expectedReturn==null) {
-                            expectedReturn = System.getProperty(HealthChecker.PROP_HEALTHCHECKER_DEF_STATUS, "OK");
-                            returnType = "httpCode200";
-                        }
-
-                        boolean isOk = false;
-                        try {
-                            isOk = tester.withUrl(fullUrl)
-                                         .withHost(host)
-                                         .withHealthCheckPath(healthCheckPath)
-                                         .withReturn(returnType, expectedReturn)
-                                         .connect();
-
-                        } catch (RuntimeException | InterruptedException | ExecutionException e) {
-                            logger.ifPresent(log -> log.debug(e));
-                        }
-
-                        if (isOk) {
-                            ((Backend) backend).setHealth(Health.HEALTHY);
-                        } else {
-                            ((Backend) backend).setHealth(Health.DEADY);
-                        }
-                        if (((Backend) backend).getHealth()!=lastHealth) {
-                            if (isOk) {
-                                logger.ifPresent(log -> log.info(url+" is OK"));
-                            } else {
-                                logger.ifPresent(log -> log.warn(url+" FAIL"));
-                            }
-                            distributedMap.getMap(Backend.class.getName()).put(backend.getId(), backend);
-                        }
-                    }
-                });
-        }
+        backendPoolCollection.stream().forEach(pool -> {
+            checkBackendPool(pool, getProperties(pool));
+        });
 
         logger.ifPresent(log -> log.debug("Job HealthCheck done."));
 
+    }
+
+    private void notifyHealthOnCheck(Entity entity, Health lastHealth, boolean isOk) {
+        if (entity instanceof Backend) {
+            Backend backend = (Backend)entity;
+            if (isOk) {
+                backend.setHealth(Health.HEALTHY);
+            } else {
+                backend.setHealth(Health.DEADY);
+            }
+            if (backend.getHealth()!=lastHealth) {
+                String hostWithPort = backend.getId();
+                if (isOk) {
+                    logger.ifPresent(log -> log.info(hostWithPort+" is OK"));
+                } else {
+                    logger.ifPresent(log -> log.warn(hostWithPort+" is FAILED"));
+                }
+                distributedMap.getMap(Backend.class.getName()).put(hostWithPort, backend);
+            }
+        }
+    }
+
+    private Map<String, Object> getProperties(final Entity pool) {
+        final Map<String, Object> properties = new HashMap<>(pool.getProperties());
+        final String hcBody = Optional.ofNullable((String) properties.get(PROP_HEALTHCHECK_RETURN))
+                                        .orElse(System.getProperty(HealthChecker.PROP_HEALTHCHECKER_DEF_BODY, ""));
+        final String hcPath = Optional.ofNullable((String) properties.get(PROP_HEALTHCHECK_PATH))
+                                        .orElse(System.getProperty(HealthChecker.PROP_HEALTHCHECKER_DEF_PATH, "/"));
+        final String hcHost = Optional.ofNullable((String) properties.get(PROP_HEALTHCHECK_HOST))
+                                        .orElse(getHost(pool));
+        final String hcStatusCode = Optional.ofNullable((String) properties.get(PROP_HEALTHCHECK_CODE))
+                                        .orElse("0");
+        properties.put(PROP_HEALTHCHECK_RETURN, hcBody);
+        properties.put(PROP_HEALTHCHECK_PATH, hcPath);
+        properties.put(PROP_HEALTHCHECK_HOST, hcHost);
+        try {
+            properties.put(PROP_HEALTHCHECK_CODE, Integer.parseInt(hcStatusCode));
+        } catch (Exception e) {
+            properties.put(PROP_HEALTHCHECK_CODE, 0);
+        }
+        return Collections.unmodifiableMap(properties);
+    }
+
+    private void checkBackendPool(final Entity pool, final Map<String, Object> properties) {
+
+        final String hcBody = (String) properties.get(PROP_HEALTHCHECK_RETURN);
+        final String hcPath = (String) properties.get(PROP_HEALTHCHECK_PATH);
+        final String hcHost = (String) properties.get(PROP_HEALTHCHECK_HOST);
+        final int statusCode = (int) properties.get(PROP_HEALTHCHECK_CODE);
+
+        farm.getCollection(Backend.class).stream()
+                .filter(backend -> backend != null && pool.getId().equals(backend.getParentId()))
+                .forEach(backend ->
+        {
+            if (backend instanceof Backend) {
+                final String hostWithPort = backend.getId();
+                final Backend.Health lastHealth = ((Backend) backend).getHealth();
+                final String fullPath = hostWithPort+hcPath;
+                boolean isOk = tester.reset()
+                                     .withUrl(fullPath)
+                                     .withHost(hcHost)
+                                     .withStatusCode(statusCode)
+                                     .withBody(hcBody)
+                                     .check();
+                notifyHealthOnCheck(backend, lastHealth, isOk);
+            }
+        });
+    }
+
+    private String getHost(Entity pool) {
+        Optional<Entity> rule = farm.getCollection(Rule.class).stream()
+                .filter(r -> pool.getId().equalsIgnoreCase((String) r.getProperty(Rule.PROP_TARGET_ID)))
+                .findAny();
+        return rule.isPresent() ? rule.get().getParentId() : "";
     }
 
 }
